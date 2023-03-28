a40114e7ec08e2c31b40cf33f4dbdb223cd39be1
Your Name <you@example.com>
2023-03-29 00:31:30 +0530

    upload7

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <Link to={"/"} className="btn btn-ghost normal-case text-xl">
-          EVA
+          EVA AI
         </Link>
       </div>
       <div className="flex-none px-4">
