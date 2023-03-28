ea227bfacf49b8fc7dba86dbaf570a05392630de
Your Name <you@example.com>
2023-03-29 00:36:19 +0530

    upload8

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/v1/openai", require("./routes/openAIroute"));
 //static files
 app.use(express.static(path.join(__dirname,'./client/build')))
 app.get('*',function(req,res){
-  res.sendFile('index.html')
+  res.send(path.join(__dirname,'./client/build/index.html'))
 })
 
 app.listen(PORT, () => {
